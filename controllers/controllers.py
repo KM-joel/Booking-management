c45b24b1b8194eb5ce7156908e2bdd3d07921488
K.joel <joelkabamba35@gmail.com>
2022-05-23 18:18:00 +0100

    [IMP] Portal detail reservation

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -13,15 +13,15 @@ class PortalReservation(CustomerPortal):
         values['reservations_count'] = reservations_count
         return values
 
-    def _application_get_page_view_values(self, application, access_token, **kwargs):
+    def _application_get_page_view_values(self, reservation, access_token, **kwargs):
         values = {
-            'page_name': 'application',
-            'application': application,
+            'page_name': 'reservation',
+            'reservation': reservation,
         }
-        return self._get_page_view_values(application, access_token, values, 'my_applications_history', False, **kwargs)
+        return self._get_page_view_values(reservation, access_token, values, 'my_applications_history', False, **kwargs)
 
 
-    @http.route(['/my/reservation/', '/my/reservation/page/<int:page>'], type='http', auth='user', website=True)
+    @http.route(['/my/reservation', '/my/reservation/page/<int:page>'], type='http', auth='user', website=True)
     def portal_my_reservations(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
 
         values = self._prepare_portal_layout_values()
@@ -31,7 +31,7 @@ class PortalReservation(CustomerPortal):
 
         searchbar_sortings = {
             'date': {'label': _('Newest'), 'order': 'create_date desc'},
-            'name': {'label': _('Description'), 'order': 'name desc'},
+            'name': {'label': _('Description'), 'order': 'reference desc'},
             'state': {'label': _('Status'), 'order': 'state'},
         }
         if not sortby:
@@ -44,7 +44,7 @@ class PortalReservation(CustomerPortal):
         reservations_count = reservations.search_count(domain)
 
         pager = portal_pager(
-            url = '/my/reservation/',
+            url = '/my/reservation',
             url_args = {'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby},
             total = reservations_count,
             page = page,
@@ -59,10 +59,61 @@ class PortalReservation(CustomerPortal):
             'reservations': reservations,
             'page_name': 'application',
             'pager': pager,
-            'default_url': '/my/reservation/',
+            'default_url': '/my/reservation',
             'searchbar_sortings': searchbar_sortings,
             'sortby': sortby,
         })
 
         return r.render('booking_management.portal_my_reservations', values)
+
+    @http.route('/reservation/request/', type='http', auth='user', website=True)
+    def view_reservation_form_create(self, error=None, **kwargs):
+        articles = r.env['product.product'].search([])
+        clients = r.env['res.users'].search([('id', '=', r.env.uid)])
+        reservations = r.env['booking.management.reservation'].search([])
+
+        values = {
+            'articles': articles,
+            'reservations': reservations,
+            'error': error,
+        }
+        return r.render('booking_management.reservation_submit', values)
+
+    @http.route('/reservation/submit_request', type='http', auth='user', website=True)
+    def create_reservation_records(self, **post):
+        try:
+            values = {
+                'client_id': r.env.uid,
+                'article_id': r.env['product.product'].sudo().search([('id', '=', post.get('type_article'))], limit=1).id,
+                'reservation_date': post.get('reservation_date'),
+                'end_date_reservation': post.get('end_date_reservation'),
+                'reservation_duration_hours': post.get('reservation_duration_hours'),
+                'reservation_duration_day': post.get('reservation_duration_day'),
+                'reservation_duration_month': post.get('reservation_duration_month'),
+            }
+            recs = r.env['booking.management.reservation'].sudo().create(values)
+            return self.portal_my_reservations()
+        except Exception as e:
+            return self.view_reservation_form_create(error=e)
+
+    @http.route(['/my/reservation/<int:reservation_id>', '/my/reservation/<int:reservation_id>/<access_token>'], type='http',
+                auth='public', website=True)
+    def open_view_detail_reservation(self, reservation_id=None, access_token=None, **kw):
+        try:
+            reservation_sudo = self._document_check_access('booking.management.reservation', reservation_id, access_token)
+        except (AccessError, MissingError):
+            return r.redirect('/my')
+
+        values = self._application_get_page_view_values(reservation_sudo, access_token, **kw)
+
+        # reservations = r.env['booking.management.reservation'].search([])
+
+        values.update({
+            'reservation': reservation_sudo,
+        })
+        print('+++++++++++++++++++++++++++++++++', values)
+        return r.render('booking_management.portal_reservation_page', values)
+
+
+
 
