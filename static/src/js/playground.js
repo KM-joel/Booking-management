b3f7eca22a483ca48bc247da72c9f52de5eaff5f
KM-joel <joelkabamba35@gmail.com>
2023-03-22 17:29:09 +0000

    [UPD] test pylint

--- static/src/js/playground.js
+++ static/src/js/playground.js
@@ -1,11 +1,10 @@
 /** @odoo-module **/
 
 import {Card} from "../components/card/card";
+import {Component} from "@odoo/owl";
 import {Counter} from "../components/counter/counter";
 import {TodoList} from "../components/todo_list/todo_list";
-import {Component} from "@odoo/owl";
 
-export class Playground extends Component {
-  static template = "booking_management.playground";
-  static components = {Card, Counter, TodoList};
-}
+export class Playground extends Component {}
+Playground.template = "booking_management.playground";
+Playground.components = {Card, Counter, TodoList};
